64fdf78de9a2f70ca0d3e693668c0bccaa348fb5
Clueless Tech <gpriyank42@gmail.com>
2021-09-22 09:13:52 +0530

    Added code to convert response and insert data using API call

--- app/src/main/java/com/cluelesstech/mysqlcurdoperation/InsertActivity.java
+++ app/src/main/java/com/cluelesstech/mysqlcurdoperation/InsertActivity.java
@@ -1,7 +1,6 @@
 package com.cluelesstech.mysqlcurdoperation;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
@@ -9,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -18,8 +19,11 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class InsertActivity extends AppCompatActivity {
@@ -58,18 +62,48 @@ public class InsertActivity extends AppCompatActivity {
                 StrictMode.setThreadPolicy(policy);
 
                 try {
-                    HttpClient httpClient = new DefaultHttpClient();
-                    HttpPost httpPost = new HttpPost("");
-                    httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-                    HttpResponse response = httpClient.execute(httpPost);
+                    HttpClient httpclient = new DefaultHttpClient();
+
+                    HttpPost httppost = new HttpPost("http://192.168.0.182/MySQLCURDOperation/insert.php"); //YOUR PHP SCRIPT ADDRESS
+                    httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                    HttpResponse response = httpclient.execute(httppost);
+
                     HttpEntity httpEntity = response.getEntity();
+
                     inputStream = httpEntity.getContent();
 
-                    Log.e("log_pass", "Connection Success");
-                    Toast.makeText(getApplicationContext(), "Record Inserted ", Toast.LENGTH_LONG).show();
+                    Log.e("log_pass", inputStream.toString());
+                    // Toast.makeText(getApplicationContext(), "Record Inserted ", Toast.LENGTH_LONG).show();
                 } catch (Exception e) {
                     Log.e("log_error", "Error in http connection" + e.toString());
                     Toast.makeText(getApplicationContext(), "Error in connection", Toast.LENGTH_LONG).show();
+                }
+
+                //convert response to string
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"), 8);
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line + " \n");
+                        Intent i = new Intent(getBaseContext(), MainActivity.class);
+                        startActivity(i);
+                    }
+                    inputStream.close();
+
+                    result = stringBuilder.toString();
+                    Log.e("log_result", result);
+                } catch (Exception e) {
+                    Log.e("log_Error", "Error converting result" + e.toString());
+                }
+
+                try {
+                    JSONObject json_data = new JSONObject(result);
+                    CharSequence charSequence = (CharSequence) json_data.get("re");
+                    Toast.makeText(getApplicationContext(), charSequence, Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+
+                    Toast.makeText(getApplicationContext(), "Error parsing data", Toast.LENGTH_LONG).show();
                 }
             }
         });
